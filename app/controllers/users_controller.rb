d97f320dfc6ccb04b5e2370f6f2381783b6e0d43
isseistar <issei0813star@gmail.com>
2021-08-07 04:41:01 +0900

    ユーザー登録機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 登録成功
+    else
+      # 登録失敗
+      render "new"
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+    
 end
--- test/integration/sign_up_test.rb
+++ test/integration/sign_up_test.rb
@@ -2,4 +2,41 @@ require "test_helper"
 
 class SignUpTest < ActionDispatch::IntegrationTest
   
+  test "Invalid signup information name" do
+    get signup_path
+    assert_template "users/new"
+    assert_no_difference "User.count" do
+      post users_path, params: {user:{name: "", email: "example@example.com",
+      password: "password", password_confirmation: "password"}}
+    end
+    assert_template "users/new"
+  end
+
+  test "Invalid signup information email" do
+    get signup_path
+    assert_template "users/new"
+    assert_no_difference "User.count" do
+      post users_path, params: {user:{name: "name", email: "",
+      password: "password", password_confirmation: "password"}}
+    end
+    assert_template "users/new"
+  end
+
+  test "Invalid signup information password" do
+    get signup_path
+    assert_template "users/new"
+    assert_no_difference "User.count" do
+      post users_path, params: {user:{name: "", email: "example@example.com",
+      password: "pass", password_confirmation: "password"}}
+    end
+    assert_template "users/new"
+  end
+
+  test "valid signup information" do
+    assert_difference "User.count +1" do
+      post users_path, params: {user:{name:"name", email: "example@example.com",
+      password: "password", password_confirmation: "password"}}
+    end
+  end
+  
 end
