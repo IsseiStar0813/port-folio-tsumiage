55e3a68e5d565e53da54f93bb349a6c3e61578d1
isseistar <issei0813star@gmail.com>
2021-08-07 21:20:45 +0900

    ログイン機能実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン成功
+      log_in(user)
+      flash[:success] = "ログインしました"
+      redirect_to user
+    else
+      # ログイン失敗
+      flash.now[:danger] = "メールアドレスかパスワードが間違っています"
+      render "sessions/new"
+    end
+  end
+end
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:issei)
+  end
+
+  test "login with invalid information" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
+end
