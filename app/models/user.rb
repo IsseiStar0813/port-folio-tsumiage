a420415be87474ebe07cef0c098f6058e9a6759b
isseistar <issei0813star@gmail.com>
2021-08-08 14:04:02 +0900

    ユーザー登録時のpasswordの不具合を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
     validates :name, presence: :true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: {maximum: 255}, uniqueness: true
-    validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
+    validates :email, presence: true, length: {maximum: 255}, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
     has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :password, length: { minimum: 6 }, presence: true
+    validates :password_confirmation, presence: true
+    
 
+    
     # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
