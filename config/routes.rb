24204c0b8db2453e22fbfda93061f24291390a09
isseistar <issei0813star@gmail.com>
2021-08-08 14:26:34 +0900

    ユーザー情報更新用のフォーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   root "static_pages#home"
   get "/signup", to: "users#new"
-  resources :users
+  resources :user, :except => :index
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
