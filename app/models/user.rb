3322c5af09c6eedac0c364637024f4d4e2fad4e7
isseistar <issei0813star@gmail.com>
2021-08-08 14:10:09 +0900

    ユーザーモデルのエラーメッセー化の改良

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     validates :name, presence: :true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: {maximum: 255}, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    validates :email, presence: true, length: {maximum: 255}, uniqueness: true 
+    validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
     has_secure_password
     validates :password, length: { minimum: 6 }, presence: true
     validates :password_confirmation, presence: true
