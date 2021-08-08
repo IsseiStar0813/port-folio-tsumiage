22d096353e5dbbdbfed77bbdcb329188d0bb8b22
isseistar <issei0813star@gmail.com>
2021-08-08 09:08:01 +0900

    ログイン時にレイアウトを変更

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,4 +5,16 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ログイン中のユーザーがいれば、そのユーザーを返す
+  def current_user
+    if session[:user_id]
+      return User.find(session[:user_id])
+    end
+  end
+
+  # ユーザーがログイン中かどうか判断する
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :users
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  delete "logput", to: "sessions#destroy"
+  delete "logout", to: "sessions#destroy"
 end
