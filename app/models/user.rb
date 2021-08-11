92d796dabab9b0eb5aa4182c547c23f1ebd35557
isseistar <issei0813star@gmail.com>
2021-08-11 23:35:28 +0900

    beforeアクションをユーザーモデルに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
     validates :name, presence: :true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 255}, uniqueness: true 
@@ -15,5 +18,25 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = " ログインしてください"
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーか確認
+    def correct_user
+      @user = User.find(params[:id])
+      unless @user == current_user
+        flash[:danger] = "自分以外の情報は確認できません"
+        redirect_to root_url
+      end
+    end
+
 
 end
