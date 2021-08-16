884259f73510bde03d8376eb70288b7aa5f1da95
isseistar <issei0813star@gmail.com>
2021-08-17 02:34:54 +0900

    microposts コントローラーのdestroy とcreateアクションを、ログイン中のユーザーのみ使えるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,14 +50,6 @@ class UsersController < ApplicationController
 
   # beforeアクション
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = " ログインしてください"
-        redirect_to login_url
-      end
-    end
-
     # 正しいユーザーか確認
     def correct_user
       @user = User.find(params[:id])
