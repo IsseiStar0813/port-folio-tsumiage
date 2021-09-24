686448c9f77b07f04ab295be214b87a37c36b587
isseistar <issei0813star@gmail.com>
2021-09-24 20:48:38 +0900

    remember_token作成用のメソッド,

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,21 @@ class User < ApplicationRecord
 
     
   # 渡された文字列のハッシュ値を返す
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを作成
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 作成したトークンをデータベースに保存
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   # 全ユーザーのalready_postedをfalse(未投稿の状態)にする
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_115143) do
+ActiveRecord::Schema.define(version: 2021_09_24_113956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_09_17_115143) do
     t.string "password_digest"
     t.integer "tsumiage_count", default: 0
     t.boolean "already_posted", default: false, null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
