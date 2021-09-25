d01669c8e2bc1aea3012b93decdbcff7e9d129ea
isseistar <issei0813star@gmail.com>
2021-09-25 21:03:50 +0900

    アカウント有効トークンを、ユーザーが作成される前に定義するメソッド作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token
   before_save :downcase_email
+  before_create :create_activation_digest
   has_many :posts, dependent: :destroy
 
     validates :name, presence: :true, length: {maximum: 50}
@@ -15,6 +16,12 @@ class User < ApplicationRecord
   # メールアドレスをすべて小文字にする
   def downcase_email
     self.email = email.downcase
+  end
+
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
     
   # 渡された文字列のハッシュ値を返す
