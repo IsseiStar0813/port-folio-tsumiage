46a0bce7da76e0d0d47d742394dc2e9fc68a8ea4
isseistar <issei0813star@gmail.com>
2021-09-26 23:59:24 +0900

    sessions コントローラのcreateアクションを修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      if user.account_activation
+      if user.activated?
       # ログイン成功
       log_in(user)
       remember user
