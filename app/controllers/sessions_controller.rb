84796560fbb5adba008bf977e60c796c6bb3074f
isseistar <issei0813star@gmail.com>
2021-09-26 23:56:42 +0900

    アカウントが有効化されている場合のみログインできるように変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      if user.account_activation
       # ログイン成功
       log_in(user)
       remember user
       flash[:success] = "ログインしました"
       redirect_to user
+      else
+        # アカウントが有効化されていない場合
+        flash[:warning] = "メールを確認し、アカウントを有効化してください"
+        redirect_to root_url
+      end
     else
       # ログイン失敗
       flash.now[:danger] = "メールアドレスかパスワードが間違っています"
