3fd90b49578934f16e104b2c30f18fc11b3ea993
isseistar <issei0813star@gmail.com>
2021-09-28 02:05:11 +0900

    パスワード再設定用のメイラーの設定

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,21 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email:params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "パスワード再設定用のメールを送信しました"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "メールアドレスが登録されていないか、間違っています"
+      render 'new'
+    end
+  end
+
   def edit
   end
+
+
 end
