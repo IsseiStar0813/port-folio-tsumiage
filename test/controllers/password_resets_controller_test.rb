78955e5757e13fc3bfb78b1e74515e827a925ecf
isseistar <issei0813star@gmail.com>
2021-10-01 00:07:17 +0900

    パスワードリセットの統合テスト修正

--- test/controllers/password_resets_controller_test.rb
+++ test/controllers/password_resets_controller_test.rb
@@ -49,19 +49,19 @@ class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
           params: { email: user.email,
                     user: { password:              "foobaz",
                             password_confirmation: "barquux" } }
-    assert_select 'div#error_explanation'
+    assert_template "shared/_error_message"
     # パスワードが空
     patch password_reset_path(user.reset_token),
           params: { email: user.email,
                     user: { password:              "",
                             password_confirmation: "" } }
-    assert_select 'div#error_explanation'                       
+    assert_template "shared/_error_message"                 
     # 有効なパスワードとパスワード確認
     patch password_reset_path(user.reset_token),
           params: { email: user.email,
                     user: { password:              "foobaz",
                             password_confirmation: "foobaz" } }
-    assert is_logged_in?
+    assert logged_in?
     assert_not flash.empty?
     assert_redirected_to user
   end
