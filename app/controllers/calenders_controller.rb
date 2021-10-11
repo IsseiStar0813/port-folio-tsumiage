0c9ef67e556cae03f8740ba321203721c083c700
isseistar <issei0813star@gmail.com>
2021-10-12 00:29:42 +0900

    カレンダー表示

--- app/controllers/calenders_controller.rb
+++ app/controllers/calenders_controller.rb
@@ -1,3 +1,7 @@
 class CalendersController < ApplicationController
   
+  def index 
+    @posts = Post.all
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   before_save :downcase_email
   before_create :create_activation_digest
   has_many :posts, dependent: :destroy 
-  has_many :calenders, dependent: :destroy
 
     validates :name, presence: :true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
