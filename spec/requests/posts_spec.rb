9025eed42a2815bb9ca96e9c240f1606b24fcbed
isseistar <issei0813star@gmail.com>
2021-12-22 22:49:57 +0900

    posts_specリファクタリング

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "GET #index", type: :request do
-  it "ログインしていないユーザーはindexアクションにアクセス不可能" do
-    get posts_path(created: 20211208)
-    expect(flash).to be_present
-    expect(response).to redirect_to login_url
-  end
 
-  it "ログインしていても,params[:created]が存在しないとindexアクションにアクセス不可能"do
-    user = FactoryBot.create(:user)
-    sign_in(user)
-    get posts_path
-    expect(response).to redirect_to user
-  end
+  let(:user) {FactoryBot.create(:user)}
 
-  it "ログインしていて、かつparams[:created]が存在する場合、indexアクションにアクセス可能"do
-    user = FactoryBot.create(:user)
-    sign_in(user)
-    get posts_path(created: 20211208)
-    expect(response).to have_http_status "200"
-  end
+  it "#index" do
+    context "ログインしていない場合、ログインページへリダイレクト" do
+      get posts_path(created: 20211208)
+      expect(flash).to be_present
+      expect(response).to redirect_to login_url
+    end
 
-  
+    context "ログインしていて,params[:created]が存在しない場合、プロフィールページへリダイレクト" do
+      sign_in(user)
+      get posts_path
+      expect(response).to redirect_to user
+    end
+
+    context "ログイン中、かつparams[:created]が存在する場合" do
+      sign_in(user)
+      get posts_path(created: 20211208)
+      expect(response).to have_http_status "200"
+    end
+   
+  end
 
 end
---
+++ spec/requests/schedules_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Schedules_controller", type: :request do
+  
+end
