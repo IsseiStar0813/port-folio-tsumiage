2de2205a567fda42666937bfe4aa3fa76a210f68
isseistar <issei0813star@gmail.com>
2022-02-13 22:29:57 +0900

    question,answerモデルのデータを新しい順に並び替え

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
   belongs_to :question
   validates :content, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
   belongs_to :user
   has_many :answers, dependent: :destroy
   validates :title, presence: true
