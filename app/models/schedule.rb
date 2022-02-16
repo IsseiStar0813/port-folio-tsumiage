0fe65f1d335bf2ed50ba6fa43539243386ca3f41
isseistar <issei0813star@gmail.com>
2022-02-16 12:05:32 +0900

    回答ページのレイアウト

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,6 @@ class Schedule < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :task, presence: true
+  validates :total_hours, presence: true
+  validates :total_minutes, presence: true
 end
