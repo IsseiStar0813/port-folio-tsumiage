a7ab2dffe05727cbc5af218c4ab48046d2ac58d8
isseistar <issei0813star@gmail.com>
2022-02-17 18:09:13 +0900

    posts#indexの不具合修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController 
   before_action :logged_in_user
-  before_action :require_created_params, only: [:index_of_the_day, :index]
+  before_action :require_created_params, only: [:index_of_the_day]
 
   def new
     @post = current_user.posts.build 
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -10,20 +10,5 @@ RSpec.describe "GET #index", type: :request do
         expect(response).to redirect_to login_url
       end
     end
-    context "ログインしていて,params[:created]が存在しない場合" do
-      it "プロフィールページへリダイレクト" do
-        sign_in(user)
-        get posts_path
-        expect(response).to redirect_to user
-      end
-    end
-    context "ログイン中、かつparams[:created]が存在する場合" do
-      it "投稿一覧ページ表示" do
-        sign_in(user)
-        get posts_path(created: 20211208)
-        expect(response).to have_http_status "200"
-        expect(response).to render_template("index")
-      end
-    end
   end
 end
